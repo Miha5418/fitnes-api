cfce13f2eff141ec533633e66118f05aa0544ca3
Karpeikin Mihail <m9963767236@gmail.com>
2022-01-19 14:23:42 +0700

    Добавил плагин maven и откатил версию gradle

---
+++ build/generated/src/main/java/ru/karpeikin/ru/fitnes/rest/admin/api/dto/ClientInfoPageable.java
@@ -0,0 +1,119 @@
+package ru.karpeikin.ru.fitnes.rest.admin.api.dto;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.ArrayList;
+import java.util.List;
+import ru.karpeikin.ru.fitnes.rest.admin.api.dto.ClientInfo;
+import ru.karpeikin.ru.fitnes.rest.admin.api.dto.PageParams;
+import org.openapitools.jackson.nullable.JsonNullable;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * ClientInfoPageable
+ */
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2022-01-19T14:23:02.915674+07:00[Asia/Novosibirsk]")
+public class ClientInfoPageable   {
+  @JsonProperty("data")
+  @Valid
+  private List<ClientInfo> data = null;
+
+  @JsonProperty("pageParams")
+  private PageParams pageParams;
+
+  public ClientInfoPageable data(List<ClientInfo> data) {
+    this.data = data;
+    return this;
+  }
+
+  public ClientInfoPageable addDataItem(ClientInfo dataItem) {
+    if (this.data == null) {
+      this.data = new ArrayList<>();
+    }
+    this.data.add(dataItem);
+    return this;
+  }
+
+  /**
+   * Get data
+   * @return data
+  */
+  @ApiModelProperty(value = "")
+
+  @Valid
+
+  public List<ClientInfo> getData() {
+    return data;
+  }
+
+  public void setData(List<ClientInfo> data) {
+    this.data = data;
+  }
+
+  public ClientInfoPageable pageParams(PageParams pageParams) {
+    this.pageParams = pageParams;
+    return this;
+  }
+
+  /**
+   * Get pageParams
+   * @return pageParams
+  */
+  @ApiModelProperty(value = "")
+
+  @Valid
+
+  public PageParams getPageParams() {
+    return pageParams;
+  }
+
+  public void setPageParams(PageParams pageParams) {
+    this.pageParams = pageParams;
+  }
+
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ClientInfoPageable clientInfoPageable = (ClientInfoPageable) o;
+    return Objects.equals(this.data, clientInfoPageable.data) &&
+        Objects.equals(this.pageParams, clientInfoPageable.pageParams);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(data, pageParams);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class ClientInfoPageable {\n");
+    
+    sb.append("    data: ").append(toIndentedString(data)).append("\n");
+    sb.append("    pageParams: ").append(toIndentedString(pageParams)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
---
+++ build/generated/src/main/java/ru/karpeikin/ru/fitnes/rest/admin/api/dto/PageParams.java
@@ -0,0 +1,133 @@
+package ru.karpeikin.ru.fitnes.rest.admin.api.dto;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.openapitools.jackson.nullable.JsonNullable;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * Параметры страницы с данными
+ */
+@ApiModel(description = "Параметры страницы с данными")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2022-01-19T14:23:02.915674+07:00[Asia/Novosibirsk]")
+public class PageParams   {
+  @JsonProperty("pageSize")
+  private Integer pageSize;
+
+  @JsonProperty("pageCurrent")
+  private Integer pageCurrent;
+
+  @JsonProperty("pageTotal")
+  private Integer pageTotal;
+
+  public PageParams pageSize(Integer pageSize) {
+    this.pageSize = pageSize;
+    return this;
+  }
+
+  /**
+   * Количество элементов на странице
+   * minimum: 1
+   * maximum: 100
+   * @return pageSize
+  */
+  @ApiModelProperty(value = "Количество элементов на странице")
+
+@Min(1) @Max(100) 
+  public Integer getPageSize() {
+    return pageSize;
+  }
+
+  public void setPageSize(Integer pageSize) {
+    this.pageSize = pageSize;
+  }
+
+  public PageParams pageCurrent(Integer pageCurrent) {
+    this.pageCurrent = pageCurrent;
+    return this;
+  }
+
+  /**
+   * Номер текущей страницы
+   * minimum: 1
+   * @return pageCurrent
+  */
+  @ApiModelProperty(value = "Номер текущей страницы")
+
+@Min(1)
+  public Integer getPageCurrent() {
+    return pageCurrent;
+  }
+
+  public void setPageCurrent(Integer pageCurrent) {
+    this.pageCurrent = pageCurrent;
+  }
+
+  public PageParams pageTotal(Integer pageTotal) {
+    this.pageTotal = pageTotal;
+    return this;
+  }
+
+  /**
+   * Общее количество страниц
+   * @return pageTotal
+  */
+  @ApiModelProperty(value = "Общее количество страниц")
+
+
+  public Integer getPageTotal() {
+    return pageTotal;
+  }
+
+  public void setPageTotal(Integer pageTotal) {
+    this.pageTotal = pageTotal;
+  }
+
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    PageParams pageParams = (PageParams) o;
+    return Objects.equals(this.pageSize, pageParams.pageSize) &&
+        Objects.equals(this.pageCurrent, pageParams.pageCurrent) &&
+        Objects.equals(this.pageTotal, pageParams.pageTotal);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(pageSize, pageCurrent, pageTotal);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class PageParams {\n");
+    
+    sb.append("    pageSize: ").append(toIndentedString(pageSize)).append("\n");
+    sb.append("    pageCurrent: ").append(toIndentedString(pageCurrent)).append("\n");
+    sb.append("    pageTotal: ").append(toIndentedString(pageTotal)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
